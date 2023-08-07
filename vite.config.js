6c96a2b3004c2b2ade78a232bea2e373bc57478e
Justice Duruanyim <uzochiduru8747@gmail.com>
2023-08-07 06:31:04 +0100

    third commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    port: 'https://todoapp-frontend-iryf.onrender.com/',
+    proxy: {
+      '/users':{
+        target:'https://todoappbackend-jz46.onrender.com',
+        changeOrigin: true
+      }
+    }
+  }
 })
