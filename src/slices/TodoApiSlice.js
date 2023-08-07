22ef8f84b4b33782bcbb9e686901139ad6bcdcfe
Justice Duruanyim <uzochiduru8747@gmail.com>
2023-08-07 07:00:13 +0100

    last commit

--- src/slices/TodoApiSlice.js
+++ src/slices/TodoApiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const todoApi = createApi({
     reducerPath: "todoApi",
-    baseQuery: fetchBaseQuery({baseUrl: ''}),
+    baseQuery: fetchBaseQuery({baseUrl: 'http://ec2-3-84-162-115.compute-1.amazonaws.com'}),
    tagTypes: ['todos'],
     endpoints: (builder) =>({
 
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 'https://radiant-frangipane-073e2d.netlify.app',
-    proxy: {
-      '/users':{
-        target:'http://ec2-3-84-162-115.compute-1.amazonaws.com/',
-        changeOrigin: true
-      }
-    }
-  }
+ 
 })
